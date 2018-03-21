e07ed752a091857f0ea690a1baf1dcdbb35d1cb9
Pete Martin <pete@pixability.com>
2018-03-20 23:04:02 -0400

    WIP, backup

---
+++ projects/boston_housing/boston_housing.py
@@ -0,0 +1,122 @@
+import numpy as np
+import pandas as pd
+from sklearn.metrics import r2_score
+from sklearn.metrics import make_scorer
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import GridSearchCV
+from sklearn.model_selection import ShuffleSplit
+from sklearn.tree import DecisionTreeRegressor
+
+
+def performance_metric(y_true, y_predict):
+    """ Calculates and returns the performance score between
+        true and predicted values based on the metric chosen. """
+
+    # TODO: Calculate the performance score between 'y_true' and 'y_predict'
+    score = r2_score(y_true, y_predict)
+
+    # Return the score
+    return score
+
+
+def fit_model(X, y):
+    """ Performs grid search over the 'max_depth' parameter for a
+        decision tree regressor trained on the input data [X, y]. """
+
+    # Create cross-validation sets from the training data
+    # sklearn version 0.18: ShuffleSplit(n_splits=10, test_size=0.1, train_size=None, random_state=None)
+    # sklearn versiin 0.17: ShuffleSplit(n, n_iter=10, test_size=0.1, train_size=None, random_state=None)
+    cv_sets = ShuffleSplit(n_splits=10, test_size=0.20, random_state=0)
+
+    # TODO: Create a decision tree regressor object
+    regressor = DecisionTreeRegressor()
+
+    # TODO: Create a dictionary for the parameter 'max_depth' with a range from 1 to 10
+    params = {'max_depth': range(1, 11)}
+
+    # TODO: Transform 'performance_metric' into a scoring function using 'make_scorer'
+    scoring_fnc = make_scorer(performance_metric)
+
+    # TODO: Create the grid search cv object --> GridSearchCV()
+    # Make sure to include the right parameters in the object:
+    # (estimator, param_grid, scoring, cv) which have values 'regressor', 'params', 'scoring_fnc', and 'cv_sets' respectively.
+    grid = GridSearchCV(estimator=regressor, param_grid=params, scoring=scoring_fnc, cv=cv_sets)
+
+    # Fit the grid search object to the data to compute the optimal model
+    grid = grid.fit(X, y)
+
+    # Return the optimal model after fitting the data
+    return grid.best_estimator_
+
+
+def PredictTrials(X, y, fitter, data):
+    """ Performs trials of fitting and predicting data. """
+
+    # Store the predicted prices
+    prices = []
+
+    for k in range(10):
+        # Split the data
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=k)
+
+        # Fit the data
+        reg = fitter(X_train, y_train)
+
+        # Make a prediction
+        pred = reg.predict([data[0]])[0]
+        prices.append(pred)
+
+        # Result
+        print "Trial {}: ${:,.2f}".format(k + 1, pred)
+
+    # Display price range
+    print "\nRange in prices: ${:,.2f}".format(max(prices) - min(prices))
+
+
+data = pd.read_csv('housing.csv')
+prices = data['MEDV']
+features = data.drop('MEDV', axis=1)
+
+print "Boston housing dataset has {} data points with {} variables each.".format(*data.shape)
+
+print "Top 5 rows:"
+print data[:5]
+
+minimum_price = prices.min()
+maximum_price = prices.max()
+mean_price = prices.mean()
+median_price = prices.median()
+std_price = prices.std()
+
+print "Statistics for Boston housing dataset:\n"
+print "Minimum price: ${:,.2f}".format(minimum_price)
+print "Maximum price: ${:,.2f}".format(maximum_price)
+print "Mean price: ${:,.2f}".format(mean_price)
+print "Median price ${:,.2f}".format(median_price)
+print "Standard deviation of prices: ${:,.2f}".format(std_price)
+
+X_train, X_test, y_train, y_test = train_test_split(features, prices, test_size=0.20, random_state=77)
+
+print "Training and testing split was successful."
+print "X_train size %s" % len(X_train)
+print "X_test size %s" % len(X_test)
+print "y_train size %s" % len(y_train)
+print "y_test size %s" % len(y_test)
+
+reg = fit_model(X_train, y_train)
+
+# Produce the value for 'max_depth'
+print "______________________________________________________________________"
+print "Parameter 'max_depth' is {} for the optimal model.".format(reg.get_params()['max_depth'])
+print ""
+
+# Produce a matrix for client data
+client_data = [[5, 17, 15], # Client 1
+               [4, 32, 22], # Client 2
+               [8, 3, 12]]  # Client 3
+
+# Show predictions
+for i, price in enumerate(reg.predict(client_data)):
+    print "Predicted selling price for Client {}'s home: ${:,.2f}".format(i+1, price)
+
+PredictTrials(features, prices, fit_model, client_data)
